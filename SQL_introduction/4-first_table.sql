a4f7f15e7c5417389a471db5433e8c38a2b20101
silassakwa <silassakwa2016@gmail.com>
2023-08-16 06:35:31 -0700

    sql:create table first table in mysql database

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,11 +3,5 @@
 */
 -- CREATE TABLE IF NOT EXISTS
 USE mysql;
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-) ENGINE=InnoDB;
-
--- Insert zero rows (this will do nothing, but table creation will still occur if needed)
-INSERT INTO first_table VALUES ();
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
 -- End of scripts
