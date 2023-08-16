c623f26b84e753f8019114a62c1fcb852167d6ef
silassakwa <silassakwa2016@gmail.com>
2023-08-16 06:24:57 -0700

    sql:create table first table in mysql database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+/*
+*Task :A script  that creates a table called first_table in the current database in your MySQL server
+*/
+-- CREATE TABLE IF NOT EXISTS
+USE mysql;
+CREATE table first_table(id INT,name varchar(255));
+-- End of scripts
