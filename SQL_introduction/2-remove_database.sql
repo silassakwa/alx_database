a22b9d35c2658550c251cc7c90fd6cb69f1a0bf2
silassakwa <silassakwa2016@gmail.com>
2023-08-16 07:12:25 -0700

    sql:drop database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,6 +1,8 @@
 /*
 *Write a script that deletes the database hbtn_0c_0 in your MySQL server.
 */
--- DROP DATABASE IN MYSQL SERVER
-DROP DATABASE hbtn_0c_0;
--- End of script
+-- deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 doesn't exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
