597b5b161ff9a5cbe2f8d0ac148bf3d9ad2ea3e7
silassakwa <silassakwa2016@gmail.com>
2023-08-17 04:20:24 -0700

    sql root user permission

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,4 @@
--- creates the MySQL server user user_0d_1.
--- user_0d_1 should have all privileges on your MySQL server
--- The user_0d_1 password should be set to user_0d_1_pwd
--- If the user user_0d_1 already exists, your script should not fail
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'user_od_1'@'localhost' IDENTIFIED BY 'user_od_pwd';
-
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
-
--- Reload privileges
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
