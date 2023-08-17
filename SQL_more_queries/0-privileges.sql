a3ccfa8a45d7353c06284f84450bcafc9d2ca8e1
silassakwa <silassakwa2016@gmail.com>
2023-08-16 23:20:14 -0700

    give user full previlleges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,5 +3,8 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Now you can list privileges for both users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
